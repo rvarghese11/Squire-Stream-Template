2d946ac42e8d75b7f3335a6a0a9b4144ec2b6419
rvarghese11 <rvarghese11@icloud.com>
2016-02-02 18:39:44 +0300

    Update app.js

--- template.stm/app.js
+++ template.stm/app.js
@@ -36,7 +36,7 @@ if (argv.m) {
 	// MODE 2 : other languages. External API code (recommended)
 	// --------------------------------------------------------
 	
-	var url = 'ENTER YOUR HTTP HERE';
+	var url = 'https://yts.ag';
 	
 	request(url, function(error, response, html) {
 	
@@ -87,7 +87,7 @@ if (argv.e) {
 	// MODE 2 : other languages. External API code (recommended)
 	// --------------------------------------------------------
 	
-	var url = 'ENTER YOUR HTTP HERE';
+	var url = 'https://yts.ag';
 	
 	request(url, function(error, response, html) {
 	
